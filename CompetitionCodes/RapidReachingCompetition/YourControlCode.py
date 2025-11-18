716414f30bdcde989b703fbaf671a769681c0568
Emily Shaw <chengyushaw@vl965-172-31-144-98.wireless.umass.edu>
2025-11-18 02:34:17 -0500

    added ik function

--- CompetitionCodes/RapidReachingCompetition/YourControlCode.py
+++ CompetitionCodes/RapidReachingCompetition/YourControlCode.py
@@ -14,7 +14,30 @@ class YourCtrl:
     self.kp = 150.0
     self.kd = 10.0
 
+    self.ee_id = mujoco.mj_name2id(self.m, mujoco.mjtObj.mjOBJ_BODY, "EE_Frame")
+
+  def getIK(self, goal, initq):
+      
+    qi = initq.copy
+    epsilon = 0.01
+
+    dx = goal-self.d.xpos[self.ee_id].copy()
+    while np.linalg.norm(dx) > epsilon:
+      jacp =  np.zeros((3,  self.m.nv)) 
+      jcar = np.zeros((3,  self.m.nv))
+      mujoco.mj_jac(self.m, self.d, jacp, jcar, point=None, body=self.ee_id)
+      J = jacp.copy()
+
+      qi = qi+0.1*np.linalg.pinv(J) @ dx
+      self.d.qpos[:] = qi
+      mujoco.mj_forward(self.m, self.d)
+      
+      dx = goal-self.d.xpos[self.ee_id].copy()
+    return qi
+
   def CtrlUpdate(self):
+      
+
     jtorque_cmd = np.zeros(6)
     for i in range(6):
         jtorque_cmd[i] = 150.0*(self.init_qpos[i] - self.d.qpos[i])  - 5.2 *self.d.qvel[i]
